91573df527b5ad53fb4d532a2ba6c063c8a7d61f
sgeorgiev18 <simeon.georgiev@fdiba.tu-sofia.bg>
2020-10-18 18:18:09 +0000

    was fun v14

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -2,11 +2,18 @@ package exercise3;
 
 public class NumberSearch {
   public static void main (String[] args) {
+    int s = 0;
     
     for(int a = 100; a < 1000; ++a) {
-      if (a % 5 == 0 && a % 6 == 0) {      
+      if (a % 5 == 0 && a % 6 == 0) {
+        if (s < 9) {      
        System.out.print(a + ", ");
-              
+       ++s;
+
+        } else {
+          s = 0;
+          System.out.println(a + ", ");
+        }              
      }
     }
   }
